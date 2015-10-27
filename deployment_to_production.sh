d777774cb7771e5ef2f21726ec64303d463c03c5
Shohei Saito <saito.ssh@gmail.com>
2015-10-27 22:43:18 +0900

    テスト変更

--- deployment_to_production.sh
+++ deployment_to_production.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # deploy to production
-curl -X POST --data-urlencode 'payload={"text": "【START】production環境へのデプロイを開始しました。"}' https://hooks.slack.com/services/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
+curl -X POST --data-urlencode 'payload={"text": "【START】production環境へのデプロイを開始しました。"}' https://hooks.slack.com/services/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
 # heroku run bin/cake migrations migrate
-curl -X POST --data-urlencode 'payload={"text": "【 END 】production環境へのデプロイを完了しました。"}' https://hooks.slack.com/services/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
+curl -X POST --data-urlencode 'payload={"text": "【 END 】production環境へのデプロイを完了しました。"}' https://hooks.slack.com/services/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
