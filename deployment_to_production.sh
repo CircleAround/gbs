737d71a0e628004f150fed03093d15892b8e4ada
Shohei Saito <saito.ssh@gmail.com>
2015-10-27 21:51:08 +0900

    テスト用のhookを使ったテスト

--- deployment_to_production.sh
+++ deployment_to_production.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # deploy to production
-curl -X POST --data-urlencode 'payload={"text": "【START】production環境へのデプロイを開始しました。"}' ${SLACK_WEBHOOK_URL}
-curl -X POST --data-urlencode 'payload={"text": "【 END 】production環境へのデプロイを完了しました。"}' ${SLACK_WEBHOOK_URL}
+curl -X POST --data-urlencode 'payload={"text": "【START】production環境へのデプロイを開始しました。"}' https://hooks.slack.com/services/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
+# heroku run bin/cake migrations migrate
+curl -X POST --data-urlencode 'payload={"text": "【 END 】production環境へのデプロイを完了しました。"}' https://hooks.slack.com/services/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
