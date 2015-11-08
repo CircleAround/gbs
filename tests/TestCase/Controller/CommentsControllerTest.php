67684565499cc3d187ac8e68f6e510b29eb8e735
ms2sato <ms2.sato@gmail.com>
2015-11-09 08:45:45 +0900

    Comment/add のテスト

--- tests/TestCase/Controller/CommentsControllerTest.php
+++ tests/TestCase/Controller/CommentsControllerTest.php
@@ -3,6 +3,7 @@ namespace App\Test\TestCase\Controller;
 
 use App\Controller\CommentsController;
 use Cake\TestSuite\IntegrationTestCase;
+use Cake\ORM\TableRegistry;
 
 /**
  * App\Controller\CommentsController Test Case
@@ -16,10 +17,33 @@ class CommentsControllerTest extends IntegrationTestCase
      * @var array
      */
     public $fixtures = [
+        'app.users',
         'app.comments',
         'app.threads',
         // 'app.actors'
     ];
+
+    /**
+     * ログイン処理のテストのためのテストアクションです。
+     * タイミングを見て別の場所へ移動する予定です。
+     * @return void
+     */
+    public function testLogin(){
+      $Users = TableRegistry::get('Users'); //テーブル作成は他でやるべきかもしれません。
+      $user = $Users->find()->first(); //fixtureの最初のユーザ（テストによって変えましょう）
+
+      $this->session(['user_id' => $user->id]); // 存在するユーザとしてログインしていることにする
+
+      $Threads = TableRegistry::get('Threads');
+      $thread = $Threads->find()->first();
+
+      // リクエスト処理
+      $this->post('/comments/add', ['body' => 'test', 'thread_id' => $thread->id]);
+
+      // 成功すればリダイレクトするはず！
+      $this->assertRedirect(['controller' => 'Threads', 'action' => 'view', $thread->id]);
+    }
+
 
     /**
      * Test index method
