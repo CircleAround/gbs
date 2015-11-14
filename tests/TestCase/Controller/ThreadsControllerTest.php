6a97cab2f88e7d8a5580a80586cb40d95978a33f
Kojima87 <taka87.itmag@gmail.com>
2015-11-15 00:21:18 +0900

    #89 ThreadControllerのテスト indexとaddの修正、viewとeditの追加

--- tests/TestCase/Controller/ThreadsControllerTest.php
+++ tests/TestCase/Controller/ThreadsControllerTest.php
@@ -24,20 +24,36 @@ class ThreadsControllerTest extends IntegrationTestCase
     ];
 
     /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->Users = TableRegistry::get('Users');
+    }
+
+    /**
      * Test index method
      *
      * @return void
      */
     public function testIndex()
     {
-        $threads = TableRegistry::get('Threads');
-        $query = $threads->find('all'); // 2レコード作成済み
-        $this->assertEquals(2, $query->count());
-
         $this->get('/threads/index');
         $this->assertResponseOk();
-        $result = $this->viewVariable('threads');
-//debug($result);
+        $threads = $this->viewVariable('threads');
+
+        // 件数をチェックする　現在は、2レコードあり
+        $this->assertEquals(2, $threads->count());
+
+        //2レコードのデータを確認する
+        $array = $threads->toArray();
+        $this->assertEquals('1-title', $array[0]['title']);
+        $this->assertEquals('1-body', $array[0]['body']);
+        $this->assertEquals('2-title', $array[1]['title']);
+        $this->assertEquals('2-body', $array[1]['body']);
     }
 
     /**
@@ -47,8 +63,20 @@ class ThreadsControllerTest extends IntegrationTestCase
      */
     public function testView()
     {
+      $this->get('/threads/view/1');
+      $this->assertResponseOk();
 
+      $thread = $this->viewVariable('thread');
+      $array = $thread->toArray();
 
+      //thread1を確認、付随したcomments全件のデータを１つずつ確認する
+      $this->assertEquals(1, $array['id']);
+      $this->assertEquals('1-title', $array['title']);
+      $this->assertEquals('1-body', $array['body']);
+
+      $this->assertEquals('thread1-comment1',$array['comments'][0]['body']);
+      $this->assertEquals('thread1-comment2',$array['comments'][1]['body']);
+      $this->assertEquals('thread1-comment3',$array['comments'][2]['body']);
     }
 
     /**
@@ -56,19 +84,16 @@ class ThreadsControllerTest extends IntegrationTestCase
      *
      * @return void
      */
-    public function testAdd()
+    public function testAddNG()
     {
-
-        $Users = TableRegistry::get('Users');
-        $user = $Users->find()->first(); 
+        //$Users = TableRegistry::get('Users');
+        $user = $this->Users->find()->first();
         $this->session(['user_id' => $user->id]);
 
         $threads = TableRegistry::get('Threads');
         $query = $threads->find('all'); // 2レコード作成済み
         $record_count = $query->count();
 
-
-       // NGなデータ
         $data = [
             'actor_id' => $user->id,
             'title' => '',
@@ -76,18 +101,27 @@ class ThreadsControllerTest extends IntegrationTestCase
         ];
 
         $this->post('/threads/add', $data);
-        
+
         // flash文言のチェック
         $this->assertResponseContains('The thread could not be saved.');
- 
-        // データが書き込まれたか確認
+
+        // データが書き込まれたか確認、NGでデータはかかれないので件数は変わらず2件
         $threads = TableRegistry::get('Threads');
         $query = $threads->find('all');
-        // NGでデータは描かれないため2件のまま
-        $this->assertEquals($record_count, $query->count());
+        // 即値で判定するのがよい、ロジックは入れない
+        $this->assertEquals(2, $query->count());
+    }
 
+    public function testAddOK()
+    {
+        //$Users = TableRegistry::get('Users');
+        $user = $this->Users->find()->first();
+        $this->session(['user_id' => $user->id]);
 
-       // OKなデータ
+        $threads = TableRegistry::get('Threads');
+        $query = $threads->find('all'); // レコード作成済み
+        $record_count = $query->count();
+
         $data = [
             'actor_id' => $user->id,
             'title' => 'zzzzzzzzzz-New Title',
@@ -95,19 +129,15 @@ class ThreadsControllerTest extends IntegrationTestCase
         ];
 
         $this->post('/threads/add', $data);
-        
-        // flash文言のチェック　失敗します。Flash->successの文言が取れていません。
-        //$this->assertResponseContains('The thread has been saved.');
- 
-        // データが書き込まれたか確認
+
+        // データが書き込まれたか確認、OKなので3件になった
         $threads = TableRegistry::get('Threads');
         $query = $threads->find('all');
-        // OKなので３件になった
-        $this->assertEquals($record_count+1, $query->count());
-        
+        // 即値で判定するのがよい、ロジックは入れない
+        $this->assertEquals(3, $query->count());
+
         // リダイレクト先はOKか
         $this->assertRedirect(['action'=>'index']);
-        
     }
 
     /**
@@ -115,9 +145,40 @@ class ThreadsControllerTest extends IntegrationTestCase
      *
      * @return void
      */
-    public function testEdit()
+    public function testEditNG()
     {
- 
+      //$Users = TableRegistry::get('Users');
+      $user = $this->Users->find()->first();
+      $this->session(['user_id' => $user->id]);
+
+      $this->post('/threads/edit/1', ['body' => '']);
+
+      // flash文言のチェック
+      $this->assertResponseContains('The thread could not be saved.');
+
+      $threads = TableRegistry::get('Threads');
+      $query = $threads->find()->where(['id' => 1]);
+
+      $array = $query->toArray();
+      $this->assertEquals('1-body', $array[0]['body']); // データは元のまま
+    }
+
+    public function testEditOK()
+    {
+      //$Users = TableRegistry::get('Users');
+      $user = $this->Users->find()->first();
+      $this->session(['user_id' => $user->id]);
+
+      $this->post('/threads/edit/1', ['body' => 'change1-body']);
+
+      $threads = TableRegistry::get('Threads');
+      $query = $threads->find()->where(['id' => 1]);
+
+      $array = $query->toArray();
+      $this->assertEquals('change1-body', $array[0]['body']);
+
+      // リダイレクト先はOKか
+      $this->assertRedirect(['action'=>'index']);
     }
 
     /**
@@ -127,6 +188,6 @@ class ThreadsControllerTest extends IntegrationTestCase
      */
     public function testDelete()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+      $this->markTestIncomplete('Not implemented yet.');
     }
 }
