d9687c7f8eb503e435640588c3865c85b0dc6b44
ms2sato <ms2.sato@gmail.com>
2015-12-15 00:53:09 +0900

    oauth -> Oauth

--- config/routes.php
+++ config/routes.php
@@ -53,7 +53,7 @@ Router::scope('/', function ($routes) {
      * ...and connect the rest of 'Pages' controller's URLs.
      */
     $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
-    $routes->connect('/oauth/github', ['controller' => 'oauth', 'action' => 'github']);
+    $routes->connect('/oauth/github', ['controller' => 'Oauth', 'action' => 'github']);
 
     /**
      * Connect catchall routes for all controllers.
