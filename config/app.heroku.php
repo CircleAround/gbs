eef75cfc51cb0fafc485b4864ae3ebc4dfec43f2
ms2sato <ms2.sato@gmail.com>
2015-12-14 22:46:07 +0900

    heroic settings

---
+++ config/app.heroku.php
@@ -0,0 +1,124 @@
+<?php
+$db = parse_url(env('CLEARDB_DATABASE_URL'));
+return [
+    'debug' => false,
+    'App' => [
+        'namespace' => 'App',
+        'encoding' => 'UTF-8',
+        'base' => false,
+        'dir' => 'src',
+        'webroot' => 'webroot',
+        'wwwRoot' => WWW_ROOT,
+        // 'baseUrl' => env('SCRIPT_NAME'),
+        'fullBaseUrl' => false,
+        'imageBaseUrl' => 'img/',
+        'cssBaseUrl' => 'css/',
+        'jsBaseUrl' => 'js/',
+        'paths' => [
+            'plugins' => [ROOT . DS . 'plugins' . DS],
+            'templates' => [APP . 'Template' . DS],
+            'locales' => [APP . 'Locale' . DS],
+        ],
+    ],
+    'Security' => [
+        'salt' => env('SALT'),
+    ],
+    'Cache' => [
+        'default' => [
+            'className' => 'Memcached',
+            'prefix' => 'myapp_cake_',
+            'servers' => [env('MEMCACHIER_SERVERS')],
+            'username' => env('MEMCACHIER_USERNAME'),
+            'password' => env('MEMCACHIER_PASSWORD'),
+            'duration' => '+1440 minutes',
+        ],
+
+        'session' => [
+            'className' => 'Memcached',
+            'prefix' => 'myapp_cake_session_',
+            'servers' => [env('MEMCACHIER_SERVERS')],
+            'username' => env('MEMCACHIER_USERNAME'),
+            'password' => env('MEMCACHIER_PASSWORD'),
+            'duration' => '+1440 minutes',
+        ],
+
+        '_cake_core_' => [
+            'className' => 'Memcached',
+            'prefix' => 'myapp_cake_core_',
+            'servers' => [env('MEMCACHIER_SERVERS')],
+            'username' => env('MEMCACHIER_USERNAME'),
+            'password' => env('MEMCACHIER_PASSWORD'),
+            'duration' => '+1 years',
+        ],
+
+        '_cake_model_' => [
+            'className' => 'Memcached',
+            'prefix' => 'myapp_cake_model_',
+            'servers' => [env('MEMCACHIER_SERVERS')],
+            'username' => env('MEMCACHIER_USERNAME'),
+            'password' => env('MEMCACHIER_PASSWORD'),
+            'duration' => '+1 years',
+        ],
+    ],
+    'Error' => [
+        'errorLevel' => E_ALL & ~E_DEPRECATED,
+        'exceptionRenderer' => 'Cake\Error\ExceptionRenderer',
+        'skipLog' => [],
+        'log' => true,
+        'trace' => true,
+    ],
+    'EmailTransport' => [
+        'default' => [
+            'className' => 'Mail',
+            // The following keys are used in SMTP transports
+            'host' => 'localhost',
+            'port' => 25,
+            'timeout' => 30,
+            'username' => 'user',
+            'password' => 'secret',
+            'client' => null,
+            'tls' => null,
+        ],
+    ],
+    'Email' => [
+        'default' => [
+            'transport' => 'default',
+            'from' => 'you@localhost',
+            //'charset' => 'utf-8',
+            //'headerCharset' => 'utf-8',
+        ],
+    ],
+    'Datasources' => [
+        'default' => [
+            'className' => 'Cake\Database\Connection',
+            'driver' => 'Cake\Database\Driver\Mysql',
+            'persistent' => false,
+            'host' => $db['host'],
+            'username' => $db['user'],
+            'password' => $db['pass'],
+            'database' => substr($db['path'], 1),
+            'encoding' => 'utf8',
+            'timezone' => 'UTC',
+            'cacheMetadata' => true,
+            'quoteIdentifiers' => false,
+        ],
+    ],
+    'Log' => [
+        'debug' => [
+            'className' => 'Cake\Log\Engine\ConsoleLog',
+            'stream' => 'php://stdout',
+            'levels' => ['notice', 'info', 'debug'],
+        ],
+        'error' => [
+            'className' => 'Cake\Log\Engine\ConsoleLog',
+            'stream' => 'php://stderr',
+            'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
+        ],
+    ],
+    'Session' => [
+        'defaults' => 'cache',
+        'handler' => [
+            'config' => 'session'
+        ]
+    ],
+  ];
